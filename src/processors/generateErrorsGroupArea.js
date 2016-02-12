03c30553011a5e3c5a4c90f49209f846da920f11
Ilya WingedFox Lebedev <ilya@lebedev.net>
2016-02-12 16:06:57 +0300

    Fixed empty error groups

--- src/processors/generateErrorsGroupArea.js
+++ src/processors/generateErrorsGroupArea.js
@@ -13,26 +13,30 @@ module.exports = function generateErrorsGroupArea(log, aliasMap, moduleMap, crea
     $process: function(docs) {
       // walk through the modules, take all error references and provide errors area
       moduleMap.forEach(function(module) {
-        docs.push(_(module.components)
+        var group = _(module.components)
           .groupBy('docType')
           .thru(function(groups) {
             if (groups.error) {
               module.components = module.components.filter(function(component) {
                 return 'error' !== component.docType;
               });
+              return {
+                id: module.id + '.error',
+                docType: 'module',
+                groupType: 'error',
+                moduleName: module.name,
+                module: module.name,
+                moduleDoc: {},
+                area: 'error',
+                name: module.name,
+                components: groups.error
+              };
             }
-            return {
-              id: module.id + '.error',
-              docType: 'module',
-              groupType: 'error',
-              moduleName: module.name,
-              module: module.name,
-              moduleDoc: {},
-              area: 'error',
-              name: module.name,
-              components: groups.error
-            };
-          }).value());
+          }).value();
+
+        if (group) {
+          docs.push(group);
+        }
       });
     }
   };
