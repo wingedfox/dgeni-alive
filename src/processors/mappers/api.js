96ab81c8c9e244ab3b61034bcfa5b4771f50fe25
Ilya WingedFox Lebedev <ilya@lebedev.net>
2016-02-13 00:13:39 +0300

    Added checks for incomplete module definitions in navigation: only module or no module defined for the components found

--- src/processors/mappers/api.js
+++ src/processors/mappers/api.js
@@ -17,43 +17,55 @@ module.exports = function navigationMapper_API(aliasMap, log) {
         debug('Processing module: %s', moduleName);
         var mod = _.find(components, {name: moduleName, module: moduleName});
 
-        var navGroup = {
-          name: moduleName,
-          type: 'groups',
-          href: mod.path, // 'api/' + moduleName, // conforms path calculation in docgen.js
-          navItems: []
-        };
-        res.push(navGroup);
+        // module is defined and not only the module
+        if (mod && components.length > 1) {
+          var navGroup = {
+            name: moduleName,
+            type: 'groups',
+            href: mod.path,
+            navItems: []
+          };
+          res.push(navGroup);
 
-        _(components)
-        .filter(function (it) {
-          return it.docType !== 'module' && it.docType !== 'overview';
-        })
-        .sortBy('docType')
-        .groupBy('docType')
-        .forEach(function (categories, typeName) {
-          var navItems = [];
-          var type = _.find(pages, {moduleName: moduleName, groupType: typeName, docType: 'componentGroup'});
+          _(components)
+          .filter(function (it) {
+            return it.docType !== 'module' && it.docType !== 'overview';
+          })
+          .sortBy('docType')
+          .groupBy('docType')
+          .forEach(function (categories, typeName) {
+            var navItems = [];
+            var type = _.find(pages, {moduleName: moduleName, groupType: typeName, docType: 'componentGroup'});
 
-          navGroup.navItems.push({
-            name: typeName,
-            type: 'section',
-            href: type.path,
-            navItems: navItems
+            navGroup.navItems.push({
+              name: typeName,
+              type: 'section',
+              href: type.path,
+              navItems: navItems
+            });
+
+            _(categories)
+              .sortBy('name')
+              .forEach(function (it) {
+                if (it.docType !== 'module') {
+                  navItems.push({
+                    name: it.name,
+                    type: it.docType,
+                    href: it.path
+                  });
+              }
+            });
           });
-
-          _(categories)
-            .sortBy('name')
-            .forEach(function (it) {
-              if (it.docType !== 'module') {
-                navItems.push({
-                  name: it.name,
-                  type: it.docType,
-                  href: it.path
-                });
-            }
-          });
-        });
+        } else {
+          if (!mod) {
+            log.error('No %s module found, related components:', moduleName);
+            components.forEach(function(c) {
+              log.error(' - ' + c.name + ' - ' + c.path);
+            });
+          } else {
+            log.warn('Module %s has no components', moduleName);
+          }
+        }
       });
     return res;
   };
