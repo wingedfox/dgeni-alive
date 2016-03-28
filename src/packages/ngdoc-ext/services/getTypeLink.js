f868416fd3cce96105cca0a06975853550273bb5
Ilya WingedFox Lebedev <ilya@lebedev.net>
2016-03-29 00:50:41 +0300

    fixed processing of function and undefined in docs

--- src/packages/ngdoc-ext/services/getTypeLink.js
+++ src/packages/ngdoc-ext/services/getTypeLink.js
@@ -13,19 +13,24 @@ module.exports = function getTypeLink(aliasMap, getLinkInfo, log) {
             log.debug('[getTypeLink] building type link for %s', typeStr);
             var type = catharsis.parse(typeName, {jsdoc: true});
             if (type.type === catharsis.Types.TypeApplication) {
-                typeName = type.applications[0].name;
+                typeName = catharsis.stringify(type.applications[0], {restringify: true});
+                type = _.clone(type);
                 type.applications = type.applications.map(function (type) {
-                    type.name = getLinkInfo(type.name).title;
+                    if (type.type !== catharsis.Types.FunctionType && type.type !== catharsis.Types.UndefinedLiteral) {
+                        type.name = getLinkInfo(type.name).title;
+                    }
                     return type;
                 });
             } else {
-                typeName = type.name;
-                type = _.clone(type);
-                type.name = getLinkInfo(type.name).title;
+                if (type.type !== catharsis.Types.FunctionType && type.type !== catharsis.Types.UndefinedLiteral) {
+                    typeName = type.name;
+                    type = _.clone(type);
+                    type.name = getLinkInfo(type.name).title;
+                }
             }
             typeStr = catharsis.stringify(type, {restringify: true});
         } catch (e) {
-            log.error('getTypeName: Parse of "%s" failed with reason: %s', typeStr, e.message);
+            log.error('[getTypeLink] Parse of "%s" failed with reason: %s', typeStr, e.message);
             typeName = typeStr;
         }
         var res = _.template('{@link ${link} ${title}}')({link: typeName, title: typeStr});
