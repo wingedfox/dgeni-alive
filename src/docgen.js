ff75fbf108cbfee218117a385a019fb03a0325bc
nhorvath <nhorvath@gmail.com>
2017-08-16 11:22:56 -0400

    remove jsx line from @wingedfox review. fix formatting.

--- src/docgen.js
+++ src/docgen.js
@@ -29,12 +29,6 @@ function configurePackage(p) {
      // build navigation
      .processor(require('./processors/structuredParam'))
 
-     /* commenting out the following because it's not a default package
-     // add the JSX file reading service to the processor
-     .config(function (readFilesProcessor, jsxFileReader) {
-        readFilesProcessor.fileReaders.unshift(jsxFileReader);
-     })*/
-
      // generate website
      .config(function(generateWebsiteProcessor) {
         generateWebsiteProcessor
